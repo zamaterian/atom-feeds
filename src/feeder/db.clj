8cfc5be424f072cc60f4c4601989d2c091e83c50
Thomas Engelschmidt <thenge@yousee.dk>
2010-11-24 15:12:56 +0100

    added insert atom-entry

--- src/feeder/db.clj
+++ src/feeder/db.clj
@@ -25,3 +25,24 @@
   (sql/with-connection feeder(sql/transaction (create-feeder )) ))
 
 
+  (defn clob-to-string [clob]
+      "Turn a Derby 10.6.1.0 EmbedClob into a String"
+      (with-open [rdr (java.io.BufferedReader. (.getCharacterStream clob))]
+            (apply str (line-seq rdr))))
+ 
+    (defn get-all [] 
+      (sql/with-connection feeder 
+         (sql/transaction
+            (sql/with-query-results rs ["select * from atoms"] 
+            (doseq [row rs] (clob-to-string (:atom row)))))))
+
+ (defn insert-atom-entry
+     "Insert data into the table"
+     [resource,atom_]
+   (sql/with-connection feeder
+     (sql/insert-values
+          :atoms
+          [:resource :atom]
+          [resource atom_])))
+
+
